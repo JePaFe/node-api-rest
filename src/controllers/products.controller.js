720334ef20ba3b5ba8b624f8ada657787986723b
Jean Paul Ferreira <jeanpaul@jepafe.net>
2025-07-03 22:13:59 -0300

    Clase 13 - Model

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,13 +1,13 @@
-import * as service from "../services/products.service.js";
+import * as model from "../models/products.model.js";
 
 export const getAllProducts = (req, res) => {
-  res.json(service.getAllProducts());
+  res.json(model.getAllProducts());
 };
 
 export const searchProduct = (req, res) => {
   const { name } = req.query;
 
-  const products = service.getAllProducts();
+  const products = model.getAllProducts();
 
   const filteredProducts = products.filter((p) =>
     p.name.toLowerCase().includes(name.toLowerCase())
@@ -19,11 +19,31 @@ export const searchProduct = (req, res) => {
 export const getProductById = (req, res) => {
   const { id } = req.params;
 
-  const product = service.getProductById(id);
+  const product = model.getProductById(id);
 
   if (!product) {
     res.status(404).json({ error: "No existe el producto" });
   }
 
   res.json(product);
+};
+
+export const createProduct = (req, res) => {
+  const { name, price } = req.body;
+
+  const newProduct = model.createProduct({ name, price });
+
+  res.status(201).json(newProduct);
+};
+
+export const deleteProduct = (req, res) => {
+  const productId = parseInt(req.params.id, 10);
+
+  const product = model.deleteProduct(productId);
+
+  if (!product) {
+    return res.status(404).json({ error: "Producto no encontrado" });
+  }
+
+  res.status(204).send();
 };
